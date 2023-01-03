fedf567225aec2e6f467a371a638b480231eea9b
Kornilov <dp310895kdi@gmail.com>
2023-01-03 19:42:11 +0200

    fix ""

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,44 @@
 import throttle from "lodash.throttle";
 const formRef = document.querySelector('.feedback-form');
-const formData = {
-};
 const STORAGE_KEY = "feedback-form-state";
-const localData = localStorage.getItem(STORAGE_KEY);
+const currentlocalStorage = JSON.parse(localStorage.getItem(STORAGE_KEY))
+const formData = {...currentlocalStorage};
 
 onCurrentContent()
 formRef.addEventListener('submit', onFormSubmit)
 formRef.addEventListener('input', throttle (onFormData, 500))
 
+function onCurrentContent() {
+    if (currentlocalStorage) {
+    if (currentlocalStorage.email !== "" && currentlocalStorage.email !== undefined) {
+        onStorageEmail()
+        }
+    if (currentlocalStorage.message !== "" && currentlocalStorage.message !== undefined) {
+        onStorageMessage()
+    }
+    };
+}
 
+function onFormData(e) {
+    formData[e.target.name] = e.target.value;
+    
+    localStorage.setItem("feedback-form-state", JSON.stringify({ ...formData }));
+};
+
+function onStorageEmail() {
+    formRef.email.value = currentlocalStorage.email;
+}
+
+function onStorageMessage() {
+    formRef.message.value = currentlocalStorage.message;
+}
 
 function onFormSubmit(e) {
     e.preventDefault();
     console.log(formData)
+    formData.message = "";
+    formData.email = "";
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY)
+
 } 
-
-function onCurrentContent() {
-    const currentlocalStorage = JSON.parse(localData)
-    if (currentlocalStorage) {      
-        if (currentlocalStorage.email !== undefined) {
-            formRef.email.value = currentlocalStorage.email;
-        }
-        if (currentlocalStorage.message !== undefined) {
-            formRef.message.value = currentlocalStorage.message;
-        }
-
-    }
-}
-
-function onFormData(e) {
-        formData[e.target.name] = e.target.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify({ ...formData }));
-};
